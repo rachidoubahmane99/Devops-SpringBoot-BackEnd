e789423de9062e9dd5e7049f2c6c869039d14ca1
rachidoubahmane99 <rachidoubahman99@gmail.com>
2021-08-04 02:39:18 +0000

    clean Tests

--- src/test/java/org/cours/VoitureRepoTest.java
+++ src/test/java/org/cours/VoitureRepoTest.java
@@ -11,12 +11,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @DataJpaTest
-//Si le test concerne uniquement les composantes JPA
-//Lorsque cette annotation est utilisée, H2, Hibernate et Spring Data sont configurés //automatiquement pour le test.
 public class VoitureRepoTest {
 @Autowired
             private TestEntityManager entityManager;
-        //TestEntityManager est utilisée pour manipuler les entités persistantes
             @Autowired
             VoitureRepo voitureRepo;
             @Test
@@ -25,9 +22,7 @@ public class VoitureRepoTest {
             Proprietaire p2 = new Proprietaire("Najat" , "ddd");
             Voiture voiture = new Voiture("MiolaCar","Uber","Blanche","M-2020",2021,180000,p1);
             entityManager.persistAndFlush(voiture);
-        //permet de faire persister ce tuple de l’entité Voiture
             assertThat(voiture.getId()).isNotNull();
-        //permet de tester qu'un tuple de Voiture a bien été ajoutée en mémoire H2
             }
             @Test
             public void supprimerVoiture() {
@@ -37,6 +32,5 @@ public class VoitureRepoTest {
             entityManager.persistAndFlush(new Voiture ("MiniCooper","Uber","Rouge","C-2020", 2021, 180000,p1));
             voitureRepo.deleteAll();
             assertThat(voitureRepo.findAll()).isEmpty();
-        //permet de tester si tous les tuples ont été supprimés
             }
 }
